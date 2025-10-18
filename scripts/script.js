b77f02897bd5fec20c7ce35de50b6467d200de77
Sergey <gricaev_sergey@mail.ru>
2025-10-18 23:19:50 +0500

    I started the video looping script

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,21 @@
 const videos = [
-	'assets/videos/clip1.mp4',
-	'assets/videos/clip2.mp4',
-	'assets/videos/clip3.mp4'
+	'assets/videos/1.mp4',
+	'assets/videos/2.mp4',
+	'assets/videos/3.mp4',
+  'assets/videos/4.mp4'
   ];
   
   let currentIndex = 0;
   const video = document.getElementById('promo-video');
   
   video.src = videos[currentIndex];
+  video.load();
   video.play();
   
   video.addEventListener('ended', () => {
 	currentIndex = (currentIndex + 1) % videos.length;
 	video.src = videos[currentIndex];
+  video.load();
 	video.play();
   });
   
